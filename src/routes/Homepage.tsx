09450439410aca753e1733b3cfac4f94bd68733b
mubaraq akanbi <akanbi.ges201@gmail.com>
2024-01-30 22:33:56 +0100

    refacor: reduced the homepage image size

--- src/routes/Homepage.tsx
+++ src/routes/Homepage.tsx
@@ -11,18 +11,20 @@ const Homepage = () => {
   }
 
   return (
-    <div className="border text-center border-gray-500 rounded-xl w-full p-32 overflow-x-hidden m-10">
-      <img
-        className="w-full h-full self-center"
-        src={homelogo}
-        alt="chat-home_image"
-      />
-      <h2 className="text-3xl self-center text-black font-bold leading-relaxed">
-        Welcome to CHATHOME
-      </h2>
-      <Link to={"login"} className="text-blue-500 texet-3xl font-semibold">
-        Continue to Login
-      </Link>
+    <div className="h-screen w-screen flex p-20 items-center justify-center">
+      <div className="border text-center border-gray-500 rounded-xl max-w-max p-10 items-center flex flex-col justify-center">
+        <img
+          className="w-fit h-fit self-center"
+          src={homelogo}
+          alt="chat-home_image"
+        />
+        <h2 className="text-3xl self-center text-black font-bold leading-relaxed">
+          Welcome to CHATHOME
+        </h2>
+        <Link to={"login"} className="text-blue-500 texet-3xl font-semibold">
+          Continue to Login
+        </Link>
+      </div>
     </div>
   );
 };
